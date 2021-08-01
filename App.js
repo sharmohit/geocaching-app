fca7f2f6d0e0490661938fecc0995b6151877f62
Mohit Sharma <43213964+sharmohit@users.noreply.github.com>
2021-08-01 04:21:59 -0400

    Added FirebaseManager and Geocache List Screen - Implemented firebase manager - Created geocache list screen

--- App.js
+++ App.js
@@ -1,21 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack'
+import { NavigationContainer} from '@react-navigation/native'
+import GeocacheListScreen from './GeocacheListScreen'
+
+const Stack = createStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="GeocacheListScreen">
+        <Stack.Screen name="GeocacheListScreen" component={GeocacheListScreen} options={{title: "Geocaching Sites"}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ GeocacheListScreen.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react'
+import { View, Text, Button, ActivityIndicator, FlatList, Pressable } from "react-native"
+import { db } from "./FirebaseManager"
+
+const GeocacheListScreen = () => {
+    return(
+        <View>
+            <Text>GeocacheListScreen</Text>
+        </View>
+    )
+}
+
+export default GeocacheListScreen
